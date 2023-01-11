189b945af06948353df4ea94fda51f4d9456b938
David Owen <dro575@gmail.com>
2023-01-11 14:57:06 -0500

    Updated Pyret lexer based on pyret-lang source.

--- pyret_lexer.py
+++ pyret_lexer.py
@@ -11,46 +11,69 @@ class PyretLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'(!|->|(?<!<)=>|\[|\]|{|}|:\s|;|,)', Punctuation),
-            (r'(\(|\)|\.|::|:=| \^ | \+ | - | \/ | \*| >= | <= |'
-             r' <> | == | <=> | =~ |=|>|<)', Operator),
-            (r'(\|)', Punctuation),
-            (r'(?<!-)(\b|^)[A-Z][A-Za-z]*(?!-)(\b|$)', Keyword.Type),
-            (r'(?x)(?<!-)(\b|^) (end|type|type-let|newtype|include|'
-             r'import|provide|provide-types|as| fun|lam|check|'
-             r'examples|is|is-not|satisfies|violates| raises|'
-             r'does-not-raise|raises-violates|raises-satisfies|'
-             r'raises-other-than| data|deriving| for|from|and|or|'
-             r'not| if|else|when|cases|ask| spy) (?!-)(\b|$)', Keyword),
-            (r'(?x)(?<!-)(\b|^) (block:|doc:|where:|with:|sharing:|'
-             r'then:|otherwise:| is==|is=~|is<=>|is-not==|is-not=~|'
-             r'is-not<=>)', Keyword),
-            (r'(?x)(?<!-)(\b|^) (var|ref|shadow|let|letrec|rec|'
-             r'method) (?!-)(\b|$)', Keyword),
-            (r'(?<!-)(\b|^)(true|false|nothing)(?!-)(\b|$)', Keyword.Constant),
+            # (r'(?!-)(\b|^)[A-Z]\w*(?!-)(\b|$)', Keyword.Type),
+                # Highlight capitalized identifiers assuming
+                # they're data types.  (Atom & VS Code
+                # highlighters do this; CPO editor does not.)
+
+            (r'(?<!-)(\b|^)(block|check|doc|else|examples|'
+             r'otherwise|provide|row|sharing|source|table|then|'
+             r'where|with)(?=:)', Keyword),
+            (r'(?<!-)(\b|^)(and|as|ascending|ask|by|cases|'
+             r'check|data|descending|do|does-not-raise|else|'
+             r'else if|end|examples|extend|extract|for|from|fun|'
+             r'hiding|if|import|include|is|is==|is=~|is-not|'
+             r'is-not==|is-not=~|is-not<=>|is-roughly|is<=>|'
+             r'because|lam|lazy|let|letrec|load-table|method|'
+             r'module|newtype|of|or|provide|provide-types|'
+             r'raises|raises-other-than|raises-satisfies|'
+             r'raises-violates|reactor|rec|ref|sanitize|'
+             r'satisfies|select|shadow|sieve|spy|order|'
+             r'transform|type|type-let|using|use|var|violates|'
+             r'when)(?!-)(\b|$)', Keyword),
+
+            (r'(?<!-)(\b|^)(true|false)(?!-)(\b|$)',
+                Name.Constant),
+                # Name.Constant, rather than Keyword.Constant,
+                # because it's more likely to be assigned a
+                # different color by pygments styles.
+                # (Atom & VS Code highlighters include
+                # "nothing" here but it isn't highlighted as
+                # a keyword by the CPO editor.)
+
+            (r'(\[|\]|{|}|\(|\)|;|\\|\.\.\.|,|->|\||:=|:)',
+                Punctuation),
+            (r'\.(?!\d)', Punctuation),
+            (r'=(?![~>])', Punctuation),
+
+            (r'(!|%)', Operator),
+            (r'(?<=\s)(\^|\+|-|\*|/|<=>|<=|>=|==|=~|<>|<|>|=>)'
+             r'(?=\s)', Operator),
+
+            (r'(?<!-)(\b|^)-?\d+([/.]\d+)?', Number),
+            (r'(?<!-)(\b|^)~-?\d+([/.]\d+)?', Number.Float),
+                # Number.Float used for Roughnum; might be
+                # assigned a different color than Number.
+
             (r"'[^']*'", String),
             (r'"[^"]*"', String),
             (r'```', String, 'multiline string'),
-            (r"'[^']*$", Error),
-            (r'"[^"]*$', Error),
-            (r'(?<![a-zA-Z0-9_-])-?[0-9]+([/.][0-9]+)?', Number),
-            (r'(?<![a-zA-Z0-9_-])~-?[0-9]+(\.[0-9]+)?', Number.Float),
-            (r'#([^|].*)?$', Comment.Single),
+
+            (r'#(?!\|).*$', Comment),
             (r'#\|', Comment.Multiline, 'multiline comment'),
 
-            (r'[^\s]', Text),
+            (r'\S', Text),
             (r'\s+', Whitespace)
         ],
+
         'multiline string': [
-            (r'[^`]+', String),
             (r'```', String, '#pop'),
-            (r'`', String)
+            (r'([^`]|`(?!`)|``(?!`))', String)
         ],
+
         'multiline comment': [
-            (r'[^#|]+', Comment.Multiline),
             (r'#\|', Comment.Multiline, '#push'),
             (r'\|#', Comment.Multiline, '#pop'),
-            (r'[|#]', Comment.Multiline)
-        ],
+            (r'([^\|]|\|(?!#))', Comment.Multiline)
+        ]
     }
-
